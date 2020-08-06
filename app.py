663c002a3e9a6899dc22f1f6b900dd1b58274b38
Raj Shah <34474021+rajshah9914@users.noreply.github.com>
2020-08-06 19:44:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,21 @@ import time
 #GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 #CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.binary_location = "/app/.apt/usr/bin/google_chrome"
+
+# driver = webdriver.Chrome(execution_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
+
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = "/app/.apt/usr/bin/google_chrome"
-
-driver = webdriver.Chrome(execution_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
-
-#chrome_options = webdriver.ChromeOptions()
-#chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-#chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--disable-dev-shm-usage")
-#chrome_options.add_argument("--no-sandbox")
-#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 ##DRIVER_PATH = 'chromedriver'
