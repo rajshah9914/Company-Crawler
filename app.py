b719e72edabe7e1f5ff064b401b3ac2ae1214fc0
Raj Shah <34474021+rajshah9914@users.noreply.github.com>
2020-08-06 19:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ from bs4 import BeautifulSoup
 import time
 
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+#GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+#CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.binary_location = "/app/.apt/usr/bin/google_chrome"
 
-driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+driver = webdriver.Chrome(execution_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
 
 #chrome_options = webdriver.ChromeOptions()
 #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
