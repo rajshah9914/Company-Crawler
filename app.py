f14d0a1f68abd9cc721ae9fa669c5557cf3b4794
Raj Shah <34474021+rajshah9914@users.noreply.github.com>
2020-08-06 22:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def tesco():
 def flipkart():
     url='https://www.flipkartcareers.com/#!/joblist'
     driver.get(url)
-    time.sleep(40)
+    time.sleep(1.6)
     content=driver.page_source
     soup1=BeautifulSoup(content,"lxml")
     xx=soup1.findAll('li',{"class": "ng-scope"})
@@ -104,7 +104,7 @@ def flipkart():
     for index in range(1,len(xx)+1):
         url_home='https://www.flipkartcareers.com/#!/joblist'
         driver.get(url_home)
-        time.sleep(20)
+        time.sleep(1.6)
         a=driver.find_element_by_link_text(str(index))
         # print(a)
         a.click()
@@ -125,7 +125,7 @@ def flipkart():
         # 'https://www.flipkartcareers.com/#!/job-view/associate-director-dc-ops-large-kolkata-kolkata-west-bengal-2019080914343317']
         for url in list_urls:
             driver.get(url)
-            time.sleep(20)
+            time.sleep(1.6)
             content = driver.page_source
             # print(content)
             # time.sleep(10)
