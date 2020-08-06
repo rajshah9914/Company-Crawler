a3cce8c1c62e39caf2c192b03d50ea49f86a80f8
Raj Shah <34474021+rajshah9914@users.noreply.github.com>
2020-08-06 18:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,12 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-
-CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
-GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
-
-
-options = Options()
-options.binary_location = GOOGLE_CHROME_BIN
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.headless = True
-
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 ##DRIVER_PATH = 'chromedriver'
